e7a6267fc130147d0c28b995ad4002cd4df3ea9f
Michael G. <95585542+Zaphryx1@users.noreply.github.com>
2024-12-11 01:34:58 -0700

    Add files via upload

---
+++ arch1-project3-lcd/game/redrawScreen.c
@@ -0,0 +1,4 @@
+
+void redrawScreen() {
+    // Placeholder for redrawScreen functionality
+}
---
+++ arch1-project3-lcd/game/wt_handler.c
@@ -0,0 +1,6 @@
+
+#include <msp430.h>
+
+void __attribute__((interrupt(WDT_VECTOR))) wt_c_handler() {
+    // Placeholder Watchdog Timer interrupt handler
+}
